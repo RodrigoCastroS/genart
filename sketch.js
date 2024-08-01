f13ed5ff95e69db372784f9e866cd2ae9c422695
Rodrigo C <rodrigocsastre@gmail.com>
2024-08-01 17:45:09 +0200

    Include noise2D filter

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ const settings = {
 };
 
 const sketch = () => {
-  const colorCount = 3;
+  const colorCount = 4;
   const palette = random.pick(palettes).slice(0, colorCount)
 
   const createGrid = () => {
@@ -21,9 +21,10 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v, 2, 1.2) * 0.03)
         points.push({
           color: random.pick(palette),
-          radius: Math.abs(random.gaussian() * 0.01),
+          radius,
           position: [u, v]
         });
       }
@@ -33,7 +34,7 @@ const sketch = () => {
 
   // setting a determined seed to keep the same randomness everytime we refresh
   // or if we want to sync it with other generative art.
-  random.setSeed(312);
+
   const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 200;
 
