64f675db6f68793f61bcaa95b091336a254dfb3c
Rodrigo C <rodrigocsastre@gmail.com>
2024-08-07 12:11:13 +0200

    Create blurry sphere

--- shader.js
+++ shader.js
@@ -17,18 +17,19 @@ const frag = glsl(/*glsl*/ `
   varying vec2 vUv;
 
   void main () {
-    vec3 colorA = sin(time) + vec3(1.0, 1.0, 0.0);
-    vec3 colorB = vec3(0.0, 1.0, 0.8);
+    vec3 colorA = sin(time) + vec3(0.2, 0.8, 0.0);
+    vec3 colorB = vec3(0.0, 0.9, 0.9);
 
     vec2 center = vUv - 0.5;
     center.x *= aspect;
     float distance = length(center);
     
-    float alpha = smoothstep(0.252, 0.25, distance);
+    // the closer the steps, the sharper the edges are going to be
+    float alpha = smoothstep(0.25777, 0.25, distance);
 
     // Mix colors from one vector to another based on the 2vector reference
-
     vec3 color = mix(colorA, colorB, cos(vUv.y) + vUv.x * sin(time)); // Include time variation to move the gradient
+    
     // set a ternary to fill the alpha channe  l with full or null opacity
     gl_FragColor = vec4(color, alpha);
   }
